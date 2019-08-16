7acb741a728dbfc9edefe91807283362e3841332
Clantigua2 <clantigua954@gmail.com>
2019-08-16 08:56:53 -0400

    adding archiver file to project

---
+++ archiver/lib/archiver.rb
@@ -0,0 +1,22 @@
+require "archiver/version"
+
+module Archiver
+  # set archived_at to current time
+  def archive
+    # in ruby when the only value is a hash you don't need curly brackets,
+    # I'm leaving them there cos I like them.
+    # set this to the current time
+    update({ archived_at: Time.now })
+  end
+
+  def restore
+    # restore to nil
+    update({ archived_at: nil})
+  end
+
+  def archived?
+    # check if archive is not empty
+    not archived_at.nil?
+  end
+
+end
